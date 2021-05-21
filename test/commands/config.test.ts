1ff24cd5ca0ea5e1cdbc77c3b5b41ae40fd8c6c5
Marc Qualie <marc@marcqualie.com>
2021-05-21 11:12:06 +0100

    Increase timeout allowance for slower tests

--- test/commands/config.test.ts
+++ test/commands/config.test.ts
@@ -5,7 +5,9 @@ describe('command', () => {
     test
     .stdout()
     .command(['config', '-c', 'ecsx-test-cluster'])
-    .it('sets the correct region', ctx => {
+    .it('sets the correct region', function (ctx) {
+      this.timeout(5000)
+
       expect(ctx.stdout).to.contain('"region": "us-east-1",')
     })
   })
