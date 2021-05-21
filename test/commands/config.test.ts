bff06844a2ac4a7ade771b92cf6cc401114fcf04
Marc Qualie <marc@marcqualie.com>
2021-05-21 11:15:51 +0100

    Use oclif timeout function

--- test/commands/config.test.ts
+++ test/commands/config.test.ts
@@ -3,11 +3,10 @@ import { expect, test } from '@oclif/test'
 describe('command', () => {
   describe('config', () => {
     test
+    .timeout(5000)
     .stdout()
     .command(['config', '-c', 'ecsx-test-cluster'])
-    .it('sets the correct region', function (ctx) {
-      this.timeout(5000)
-
+    .it('sets the correct region', async function (ctx) {
       expect(ctx.stdout).to.contain('"region": "us-east-1",')
     })
   })
