f571a7309da9d6262ddab88afa1e7974ad70485a
Marc Qualie <marc@marcqualie.com>
2021-05-21 11:19:04 +0100

    Add yarn test to CI (#13)

--- test/commands/config.test.ts
+++ test/commands/config.test.ts
@@ -3,9 +3,10 @@ import { expect, test } from '@oclif/test'
 describe('command', () => {
   describe('config', () => {
     test
+    .timeout(5000)
     .stdout()
     .command(['config', '-c', 'ecsx-test-cluster'])
-    .it('sets the correct region', ctx => {
+    .it('sets the correct region', async function (ctx) {
       expect(ctx.stdout).to.contain('"region": "us-east-1",')
     })
   })
