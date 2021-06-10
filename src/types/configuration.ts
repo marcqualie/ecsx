32609d487ba048d374667f870f4983bac72761bf
Marc Qualie <marc@marcqualie.com>
2021-06-10 23:17:54 +0100

    Configuration refactor (#24)

--- src/types/configuration.ts
+++ src/types/configuration.ts
@@ -14,6 +14,25 @@ export interface ConfigurationTaskDefinition {
   executionRoleArn: string
 }
 
+export interface ConfigurationClusterDefinition {
+  environment: string
+  project?: string
+  envVars?: KeyValuePairs
+  consoleTask?: string
+  targetGroups: Array<{
+    arn: string
+    task: string
+    port: number
+  }>
+  securityGroups: string[]
+  publicSubnets?: string[] // @deprecated: Please use subnets instead
+  privateSubnets?: string[] // @deprecated: Please use subnets instead
+  subnets?: string[] // TODO: make this required once other options are removed
+  secrets?: {
+    [name: string]: string
+  }
+}
+
 export interface ConfigurationService {
   taskDefinition: string
 }
@@ -47,30 +66,13 @@ export interface ConfiguredVariables extends Variables {
 }
 
 export interface Configuration {
-  version: string
+  version?: string // @deprecated: We have no use for this
   region: string
   accountId: string
   project: string
   variables: Variables
   clusters: {
-    [clusterName: string]: {
-      environment: string
-      project?: string
-      envVars?: KeyValuePairs
-      consoleTask: string
-      targetGroups: Array<{
-        arn: string
-        task: string
-        port: number
-      }>
-      securityGroups: string[]
-      publicSubnets?: string[] // @deprecated: Please use subnets instead
-      privateSubnets?: string[] // @deprecated: Please use subnets instead
-      subnets?: string[] // TODO: make this required once other options are removed
-      secrets?: {
-        [name: string]: string
-      }
-    }
+    [clusterName: string]: ConfigurationClusterDefinition
   }
   tasks: {
     [name: string]: ConfigurationTaskDefinition
