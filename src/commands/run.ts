6067bebf68744a273473344536df0e27fb85355e
Marc Qualie <marc@marcqualie.com>
2021-06-16 19:41:33 +0100

    Wait for one off tasks to complete

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -1,4 +1,8 @@
+/* eslint-disable no-await-in-loop */
 import { flags } from '@oclif/command'
+import { Container, Task } from '@aws-sdk/client-ecs'
+import cli from 'cli-ux'
+
 import { AwsCommand } from '../command'
 import { taskDefinitionfromConfiguration } from '../ecs/task-definition'
 import { taskFromConfiguration } from '../ecs/task'
@@ -67,13 +71,48 @@ export default class RunCommand extends AwsCommand {
     if (tasks === undefined || tasks.length === 0) {
       this.error(`Could not create task definition: ${runTaskResponse}`)
     }
+    const firstTask = tasks[0]
+    if (firstTask.taskArn === undefined) {
+      this.error('Task does not have an ARN. Please try again.')
+    }
+    const taskId = firstTask.taskArn?.replace(`arn:aws:ecs:${region}:${accountId}:task/${project}-${environment}/`, '')
+    const taskUrl = `https://${region}.console.aws.amazon.com/ecs/v2/clusters/${project}-${environment}/tasks/${taskId}/logs?region=${region}`
+    this.log(`> Image: ${dockerTag}`)
+    this.log(`> Task: ${firstTask.taskArn}`)
+    this.log(`> URL: ${taskUrl}`)
 
-    this.log(JSON.stringify({
-      arns: tasks.map(task => task.taskArn),
-      urls: tasks.map(task => {
-        const id = task.taskArn?.replace(`arn:aws:ecs:${region}:${accountId}:task/${project}-${environment}/`, '')
-        return `https://${region}.console.aws.amazon.com/ecs/v2/clusters/${project}-${environment}/tasks/${id}/logs?region=${region}`
-      }),
-    }, undefined, 2))
+    // Keep polling for updates
+    let taskStatus: string | undefined
+    let taskDetails: Task | undefined = firstTask
+    cli.action.start('', taskStatus)
+    while (taskStatus !== 'STOPPED') {
+      taskDetails = await client.describeTask(clusterName, firstTask.taskArn)
+      if (taskDetails === undefined) {
+        this.error(`Could not find task details for taskArn "${firstTask.taskArn}"`)
+      }
+      taskStatus = taskDetails.lastStatus
+      cli.action.start('', taskStatus)
+
+      // eslint-disable-next-line no-await-in-loop
+      await new Promise(resolve => setTimeout(resolve, 10000))
+    }
+    cli.action.stop(taskStatus)
+
+    // If we did not get RUNNING then the console could not started properly, this should show why
+    const firstContainer = taskDetails?.containers && taskDetails.containers[0]
+    if (firstContainer === undefined) {
+      this.error('Could not locate container defintion')
+    }
+
+    // If the container failed, we want to know why
+    const exitCode = firstContainer.exitCode
+    const stopCode = firstTask.stopCode
+    const containerReason = firstContainer.reason
+    const errorReason = taskDetails.stoppedReason
+    if (exitCode !== 0 || stopCode) {
+      this.error(`Execution failed: [${stopCode || exitCode}] ${containerReason || errorReason}`)
+    }
+
+    this.log('Success!')
   }
 }
