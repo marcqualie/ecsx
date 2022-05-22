801471e71eeffb6db1f98327ccb86e5dbed8cb4d
Marc Qualie <marc@marcqualie.com>
2022-05-22 15:21:44 +0100

    Add a test for backwards compatible string based secrets

--- test/ecs/task-definition.test.ts
+++ test/ecs/task-definition.test.ts
@@ -9,7 +9,7 @@ import { configWithVariables } from '../../src/utils/config-with-variables'
 describe('ecs', () => {
   describe('task-definition', () => {
     describe('secretsFromConfiguration', () => {
-      it('translate secrets to task definition format', () => {
+      it('translate cluster secrets to task definition format', () => {
         const { config } = configWithVariables({ clusterName: 'ecsx-test-cluster' })
         const output = secretsFromConfiguration('mocha', 'ecsx-test-cluster', config)
         expect(output).to.deep.equal([
@@ -28,6 +28,21 @@ describe('ecs', () => {
           {
             name: 'X_CLUSTER_KEY_2',
             valueFrom: 'arn:aws:secretsmanager:us-east-1:1234:secret:ecsx/app/test-xxx:X_CLUSTER_KEY_2::',
+          },
+        ])
+      })
+
+      it('translate service only secrets to task definition format', () => {
+        const { config } = configWithVariables({ clusterName: 'ecsx-test-cluster-with-string-secrets' })
+        const output = secretsFromConfiguration('mocha', 'ecsx-test-cluster-with-string-secrets', config)
+        expect(output).to.deep.equal([
+          {
+            name: 'NODE_ENV',
+            valueFrom: 'arn:aws:secretsmanager:us-east-1:1234:secret:ecsx/app/test-xxx:NODE_ENV::',
+          },
+          {
+            name: 'SOME_VAR',
+            valueFrom: 'arn:aws:secretsmanager:us-east-1:1234:secret:ecsx/app/test-xxx:SOME_VAR::',
           },
         ])
       })
