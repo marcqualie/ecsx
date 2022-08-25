7d09249cbc36880f89f477bbbee332545a22be11
Sam Kah Chiin <samkahchiin@gmail.com>
2022-08-25 16:54:45 +0800

    [Hotfix] Remove irrelevant codes

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -1,6 +1,5 @@
 import { flags } from '@oclif/command'
 import { AwsCommand } from '../command'
-import { taskDefinitionfromConfiguration } from '../ecs/task-definition'
 
 export default class Config extends AwsCommand {
   static description = 'Print out current configuration'
@@ -20,28 +19,13 @@ export default class Config extends AwsCommand {
 
   async run() {
     const { flags: { clusterKey, taskName } } = this.parse(Config)
-    const { config, variables, envVars } = this.configWithVariables({
+    const { variables, envVars } = this.configWithVariables({
       clusterKey,
       taskName,
     })
-    const { clusterName } = variables
-
-    if (clusterName === undefined || taskName === undefined) {
-      throw new Error('Could not detect $clusterName and $taskName')
-    }
-
-    const taskDefinitionInput = taskDefinitionfromConfiguration({
-      clusterName,
-      taskName,
-      variables,
-      config,
-      envVars,
-    })
-
     this.log('$variables', JSON.stringify(variables, undefined, 2))
     this.log('$envVars', JSON.stringify(envVars, undefined, 2))
     this.log(' ')
-    // this.log(JSON.stringify(config, undefined, 2))
-    this.log(JSON.stringify(taskDefinitionInput, undefined, 2))
+    this.log(JSON.stringify(config, undefined, 2))
   }
 }
