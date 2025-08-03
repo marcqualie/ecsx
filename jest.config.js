6ba61f9d58098260fd2b49aaa18bb35d9603672d
Marc Qualie <101022+marcqualie@users.noreply.github.com>
2025-08-03 17:34:05 +0100

    Replace mocha, chai, nyc with jest (#106)

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+}
--- test/ecs/task-definition.test.ts
+++ test/ecs/task-definition.test.ts
@@ -1,7 +1,3 @@
-process.env.ECSX_CONFIG_PATH = './test/ecsx.yml'
-
-import { describe } from 'mocha'
-import { expect } from 'chai'
 
 import { secretsFromConfiguration } from '../../src/ecs/task-definition'
 import { configWithVariables } from '../../src/utils/config-with-variables'
@@ -12,7 +8,7 @@ describe('ecs', () => {
       it('translate secrets to task definition format', () => {
         const { config, variables: { region } } = configWithVariables({ clusterKey: 'ecsx-test-cluster' })
         const output = secretsFromConfiguration('mocha', 'ecsx-test-cluster', config, region)
-        expect(output).to.deep.equal([
+        expect(output).toEqual([
           {
             name: 'CLUSTER_KEY_1',
             valueFrom: 'arn:aws:secretsmanager:us-east-1:1234:secret:ecsx/app/test-xxx:CLUSTER_KEY_1::',
@@ -35,7 +31,7 @@ describe('ecs', () => {
       it('translate service only secrets to task definition format', () => {
         const { config, variables: { region } } = configWithVariables({ clusterKey: 'ecsx-test-cluster-with-string-secrets', region: 'eu-central-1' })
         const output = secretsFromConfiguration('mocha', 'ecsx-test-cluster-with-string-secrets', config, region)
-        expect(output).to.deep.equal([
+        expect(output).toEqual([
           {
             name: 'NODE_ENV',
             valueFrom: 'arn:aws:secretsmanager:eu-central-1:1234:secret:ecsx/app/test-xxx:NODE_ENV::',
@@ -50,7 +46,7 @@ describe('ecs', () => {
       it('translate cluster only secrets to task definition format', () => {
         const { config, variables: { region } } = configWithVariables({ clusterKey: 'ecsx-test-cluster' })
         const output = secretsFromConfiguration('has-no-secrets', 'ecsx-test-cluster', config, region)
-        expect(output).to.deep.equal([
+        expect(output).toEqual([
           {
             name: 'CLUSTER_KEY_1',
             valueFrom: 'arn:aws:secretsmanager:us-east-1:1234:secret:ecsx/app/test-xxx:CLUSTER_KEY_1::',
